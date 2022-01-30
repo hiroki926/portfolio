28eedffe8772100c88b231a6f9b1583632f28f00
hiroki926 <81535882+hiroki926@users.noreply.github.com>
2022-01-30 21:04:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -37,13 +37,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Chela+One&display=swap" rel="stylesheet">
     
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7SN7BKJNFZ"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KWPPSK1DL4"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-7SN7BKJNFZ');
-   </script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-KWPPSK1DL4');
+    </script>
+    </script>
 </head>
 
 <body>
